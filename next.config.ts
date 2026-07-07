3783aede1f55bac0142fdd6975cbf4dcecb5cc8c
Tarun <engr.tarundilwal@gmail.com>
2026-07-07 23:57:28 +0530

    feat: add fakestoreapi.com to allowed image hostnames in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
       },
+      {
+        protocol: "https",
+        hostname: "fakestoreapi.com",
+      },
     ],
   },
 };
